41e74de1c721a20fa606574f85102d8a5564af68
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-09-16 19:35:07 +0300

    06

---
+++ homework/03/06/index.js
@@ -0,0 +1,33 @@
+async function makeCalculationAndSendStuffToBackend() {
+  function makeCalculation(a, b) {
+    function asynFunc(resolve, reject) {
+      if (b === 0) {
+        reject("cannot divide with zero.");
+      } else {
+        setTimeout(() => {
+          console.log("calculating stuff");
+          const division = a / b;
+          resolve(division);
+        }, 1000);
+      }
+    }
+    const p = new Promise(asynFunc);
+    return p;
+  }
+  const result = await makeCalculation(10, 2);
+  console.log(result);
+  function sendStuffToBackend(result) {
+    function asynFunc(resolve, reject) {
+      setTimeout(() => {
+        console.log(`sending to backend ${result}`);
+        resolve("done");
+      }, 1000);
+    }
+    const p = new Promise(asynFunc);
+    return p;
+  }
+  const msg = await sendStuffToBackend(result);
+  return msg;
+}
+
+makeCalculationAndSendStuffToBackend().then((msg) => console.log(msg));
